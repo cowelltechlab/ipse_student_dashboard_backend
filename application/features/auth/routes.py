9e3dd6de3ac080b6fd36bd4dd0d1e82970ea3ac4
Rani Hinnawi <rhinnawi95@gmail.com>
2025-06-12 19:51:44 -0700

    merged in changes from main. began base setup of routing

--- application/features/auth/routes.py
+++ application/features/auth/routes.py
@@ -1,1 +1,5 @@
+from fastapi import HTTPException, APIRouter, Depends, status, Query
+
 # This should include a way to log in through Google and generic username/password
+''' Prepend all student routes with /students and collect all student-relevant endpoints under Students tag in SwaggerUI'''
+router = APIRouter()
